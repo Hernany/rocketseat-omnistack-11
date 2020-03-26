f16c22b82ee376e7659c3cc4b9ba6db0d9812c31
Hernany Santos <integrusdev@gmail.com>
2020-03-26 10:34:11 -0300

    Implementação do Backend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+
+app.use(cors());
 
 //Informa ao Express que será trabalhado com respostas em json
 app.use(express.json())
